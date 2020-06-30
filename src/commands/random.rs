d4fd962b08b8b6c22f6d47e2b7fd003ac3c720a1
kawaemon <34652535+kawaemon@users.noreply.github.com>
2020-07-01 01:32:15 +0900

    修正#4

--- src/commands/random.rs
+++ src/commands/random.rs
@@ -1,10 +1,10 @@
 use crate::commands::meigen_format;
 use crate::commands::{Error, Result};
 use crate::db::MeigenDatabase;
+use crate::db::RegisteredMeigen;
 use crate::message_parser::ParsedMessage;
 
 pub fn random(db: &impl MeigenDatabase, message: ParsedMessage) -> Result {
-    use rand::Rng;
     let count: usize = {
         message
             .args
@@ -14,12 +14,52 @@ pub fn random(db: &impl MeigenDatabase, message: ParsedMessage) -> Result {
     };
 
     let meigen_count = db.meigens().len();
-    let mut rng = rand::thread_rng();
     let mut result = String::new();
 
-    for _ in 0..count {
-        let index = rng.gen_range(0, meigen_count);
-        result += &format!("{}\n", meigen_format(&db.meigens()[index]));
+    let rands = gen_rand_vec(count, 0, meigen_count);
+
+    let meigens = rands
+        .iter()
+        .map(|x| {
+            db.meigens()
+                .get(*x)
+                .expect("BUG: range of random values isn't fit to array's range.")
+        })
+        .collect::<Vec<_>>();
+
+    local_listify(&meigens.as_slice())
+}
+
+fn gen_rand_vec(count: usize, range_from: usize, range_to: usize) -> Vec<usize> {
+    use rand::Rng;
+
+    let mut rng = rand::thread_rng();
+
+    let mut result = vec![0; count as usize];
+    result
+        .iter_mut()
+        .for_each(|x| *x = rng.gen_range(range_from, range_to));
+
+    result
+}
+
+fn local_listify(list: &[&RegisteredMeigen]) -> Result {
+    const LIST_MAX_LENGTH: usize = 500;
+    const MAX_LENGTH_PER_MEIGEN: usize = 50;
+
+    let mut result = String::new();
+
+    for meigen in list {
+        let formatted = crate::commands::meigen_tidy_format(meigen, MAX_LENGTH_PER_MEIGEN);
+        result += &format!("{}\n", formatted);
+    }
+
+    if result.is_empty() {
+        return Err(Error::no_meigen_matches());
+    }
+
+    if result.chars().count() >= LIST_MAX_LENGTH {
+        return Err(Error::too_many_meigen_matches());
     }
 
     Ok(result)
