719926bade14561ca7993aa813563eb6cb860e89
loxygenk <loxygen.k@gmail.com>
2020-07-04 11:52:54 +0900

    Searchコマンド用のヘルプを実装した

--- src/commands/search.rs
+++ src/commands/search.rs
@@ -1,8 +1,10 @@
 mod author;
+mod help;
 mod word;
 
 pub use author::author;
 pub use word::content;
+pub use help::help;
 
 use crate::commands::{Error, Result};
 use crate::db::MeigenDatabase;
@@ -10,6 +12,7 @@ use crate::message_parser::ParsedMessage;
 
 const AUTHOR_SEARCH_COMMAND: &str = "author";
 const WORD_SEARCH_COMMAND: &str = "word";
+const SEARCH_HELP_COMMAND: &str = "help";
 
 pub fn search(db: &impl MeigenDatabase, message: ParsedMessage) -> Result {
     const LIST_MEIGEN_DEFAULT_COUNT: i32 = 5;
@@ -37,6 +40,7 @@ pub fn search(db: &impl MeigenDatabase, message: ParsedMessage) -> Result {
     match sub_command.as_str() {
         AUTHOR_SEARCH_COMMAND => author(db, search_query, show_count, page),
         WORD_SEARCH_COMMAND => content(db, search_query, show_count, page),
+        SEARCH_HELP_COMMAND => help(),
         _ => Err(Error::invalid_search_subcommand())
     }
 
---
+++ src/commands/search/help.rs
@@ -0,0 +1,16 @@
+use crate::commands::Result;
+
+const HELP_TEXT: &str = "```asciidoc
+= meigen-bot-rust (search help) =
+g!meigen search [検索内容] [表示数=5] [ページ=1]
+
+= 検索内容 =
+author :: 名言を発した人の名前から検索します
+search :: 名言の内容から検索します
+
+全ての検索コマンドが部分一致検索です。
+```";
+
+pub fn help() -> Result {
+    Ok(HELP_TEXT.to_string())
+}
