774b4e7de427e6ac22d9122f3c400f38c010ecbe
kawaemon <34652535+kawaemon@users.noreply.github.com>
2020-08-13 00:48:23 +0900

    依存のアップデートとstatusコマンドのバグ修正

--- src/commands/status.rs
+++ src/commands/status.rs
@@ -5,13 +5,7 @@ use std::sync::Arc;
 use std::sync::RwLock;
 
 pub async fn status(db: &Arc<RwLock<impl MeigenDatabase>>) -> Result {
-    let meigens = db
-        .read()
-        .unwrap()
-        .current_meigen_id()
-        .await
-        .map_err(Error::load_failed)?;
-    let text = format!("合計名言数: {}個", meigens);
+    let meigens = db.read().unwrap().len().await.map_err(Error::load_failed)?;
 
-    Ok(text)
+    Ok(format!("合計名言数: {}個", meigens))
 }
