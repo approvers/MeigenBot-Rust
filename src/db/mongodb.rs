a6e4be4fcffa21a83026004c38a44daf7b4d0bac
kawaemon <34652535+kawaemon@users.noreply.github.com>
2020-08-18 00:11:58 +0900

    mongodbのlenが動いてない問題を修正

--- src/db/mongodb.rs
+++ src/db/mongodb.rs
@@ -247,8 +247,8 @@ impl MeigenDatabase for MongoDB {
             .map_err(MongoDBError::get_fail)?
             .get("id")
             .ok_or_else(|| MongoDBError::get_fail("MongoDB response doesn't contain \"id\" field"))?
-            .as_i64()
-            .ok_or_else(|| MongoDBError::get_fail("MongoDB response's \"id\" field wasn't i64"))
+            .as_f64()
+            .ok_or_else(|| MongoDBError::get_fail("MongoDB response's \"id\" field wasn't f64"))
             .map(|x| x as u64)
     }
 
