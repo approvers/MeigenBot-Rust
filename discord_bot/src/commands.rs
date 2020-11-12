8151c631ff30ad4e56e7f4aa1196e5dc59411e85
kawaemon <34652535+kawaemon@users.noreply.github.com>
2020-11-13 02:43:53 +0900

    ✨ Remove tidying feature

--- discord_bot/src/commands.rs
+++ discord_bot/src/commands.rs
@@ -53,32 +53,8 @@ pub fn meigen_format(meigen: &RegisteredMeigen) -> String {
     internal_format(meigen.id, &meigen.author, &meigen.content)
 }
 
-fn meigen_tidy_format(meigen: &RegisteredMeigen, max_length: usize) -> String {
-    const BASE: &str = "Meigen No.\n```\n\n    --- \n```";
-    const TIDY_SUFFIX: &str = "...";
-    const NO_SPACE_MSG: &str = "スペースが足りない...";
-
-    let remain_length = (max_length as i32)
-        - (BASE.chars().count() as i32)
-        - (meigen.author.chars().count() as i32);
-
-    // 十分なスペースがあるなら、そのままフォーマットして返す
-    if remain_length >= meigen.content.chars().count() as i32 {
-        return meigen_format(meigen);
-    }
-
-    // 作者名が長すぎるなどの理由で、...を使った省略でも入らない場合は、NO_SPACE_MSGを突っ込む
-    if remain_length <= NO_SPACE_MSG.chars().count() as i32 {
-        return format!("Meigen No.{}\n```{}```", meigen.id, NO_SPACE_MSG);
-    }
-
-    // 上記どれにも引っかからない場合、最後を...で削って文字列を減らして返す
-    let content = meigen
-        .content
-        .chars()
-        .take((remain_length - TIDY_SUFFIX.len() as i32) as usize)
-        .chain(TIDY_SUFFIX.chars())
-        .collect::<String>();
-
-    internal_format(meigen.id, &meigen.author, &content)
+fn meigen_tidy_format(meigen: &RegisteredMeigen, _: usize) -> String {
+    // 前までは名言がある程度長い場合末尾を省略するようにしていたのですが
+    // どうやら余計な機能だったようなので削除しました。
+    return meigen_format(meigen);
 }
