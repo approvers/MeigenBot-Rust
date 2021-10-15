c727b75d22d27386cc6881872dddeceec99cace4
kawaemon <me@kawaemon.dev>
2021-10-15 13:04:48 +0900

    feat: grpc bin

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
-#[cfg(feature = "api_grpc")]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    #[cfg(feature = "api_grpc")]
     tonic_build::compile_protos("proto/meigen_api.proto")?;
     Ok(())
 }
